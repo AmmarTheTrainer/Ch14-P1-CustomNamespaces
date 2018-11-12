6d80e46a65fdc7984da414f80f5b14c7de87cc71
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-11-12 16:30:16 +0500

    Final Code update with git ignore

---
+++ Ch14-P1-CustomNamespaces/Circle2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace Lecture14
+namespace Lecture14.My3DShapes
+{ 
+    //namespace My3DShapes
+    //{
+        // 3D Circle class.
+        public class Circle { }
+        // 3D Hexagon class.
+        public class Hexagon { }
+        // 3D Square class.
+        public class Square { }
+    //}
+}
--- Ch14-P1-CustomNamespaces/Program.cs
+++ Ch14-P1-CustomNamespaces/Program.cs
@@ -4,6 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using MyShapes;
+//using My3DShapes;
+//using Lecture14.
+//using My3DHexagon = My3DShapes.Hexagon;
+using My3DHexagon = Lecture14.My3DShapes.Hexagon;
+
+//using bfHome = System.Runtime.Serialization.Formatters.Binary;
+//using bfHome = System.Runtime.Serialization.Formatters.Binary.BinaryFormatter;
+//using System.Runtime.Serialization.Formatters.Binary;
+    
 namespace Ch14_P1_CustomNamespaces
 {
     class Program
@@ -11,6 +21,26 @@ namespace Ch14_P1_CustomNamespaces
         static void Main(string[] args)
         {
 
+            //Hexagon h = new Hexagon();
+            //Circle c = new Circle();
+            //Square s = new Square();
+
+            //    MyShapes.Hexagon h = new MyShapes.Hexagon();
+            //    MyShapes.Circle c = new MyShapes.Circle();
+            //    MyShapes.Square s = new MyShapes.Square();
+
+            //My3DShapes.Hexagon h = new My3DShapes.Hexagon();
+            //My3DShapes.Circle c = new My3DShapes.Circle();
+            //MyShapes.Square s = new MyShapes.Square();
+
+            My3DHexagon hexagon = new My3DHexagon();
+
+
+            //bfHome.BinaryFormatter b = new bfHome.BinaryFormatter();
+            //bfHome b = new bfHome();
+            //BinaryFormatter b = new BinaryFormatter();
+
+            Circle c = new Circle();
         }
     }
 }
